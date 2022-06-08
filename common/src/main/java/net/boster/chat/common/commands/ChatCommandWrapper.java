0e89b4b4557505721a0e609965c448c08202c4d8
Bosternike <aav0918@gmail.com>
2022-06-08 19:40:31 +0200

    t

--- common/src/main/java/net/boster/chat/common/commands/ChatCommandWrapper.java
+++ common/src/main/java/net/boster/chat/common/commands/ChatCommandWrapper.java
@@ -2,6 +2,7 @@ package net.boster.chat.common.commands;
 
 import lombok.Getter;
 import net.boster.chat.common.BosterChatPlugin;
+import net.boster.chat.common.config.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class ChatCommandWrapper implements ChatCommand {
@@ -14,5 +15,16 @@ public abstract class ChatCommandWrapper implements ChatCommand {
         this.plugin = plugin;
         this.name = name;
         this.aliases = aliases;
+    }
+
+    public ChatCommandWrapper(@NotNull BosterChatPlugin plugin, @NotNull ConfigurationSection section) throws NullPointerException {
+        this.plugin = plugin;
+
+        if(section.getString("name") == null) {
+            throw new NullPointerException("Command name can not be null!");
+        }
+
+        this.name = section.getString("name");
+        this.aliases = section.getStringList("aliases").toArray(new String[]{});
     }
 }
